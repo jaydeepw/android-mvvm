a3bf26d09088f082edfa9144d83f05bee086d52e
jaydeepw <jaydeep.w@gmail.com>
2019-07-04 10:58:23 +0530

    feature: Show filter menu in the fragment

--- MatchFilter/app/src/main/java/com/github/jaydeepw/matchfilter/views/MainFragment.kt
+++ MatchFilter/app/src/main/java/com/github/jaydeepw/matchfilter/views/MainFragment.kt
@@ -1,12 +1,15 @@
 package com.github.jaydeepw.matchfilter.views
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import com.github.jaydeepw.matchfilter.R
 
-class MainFragment() : BaseFragment() {
+class MainFragment : BaseFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,10 +19,9 @@ class MainFragment() : BaseFragment() {
         return inflater.inflate(R.layout.fragment_list, null, false)
     }
 
-    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        // super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu_main, menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -27,8 +29,8 @@ class MainFragment() : BaseFragment() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_filter-> true
             else -> super.onOptionsItemSelected(item)
         }
-    }*/
+    }
 }
