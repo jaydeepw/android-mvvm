f4e47d43a2951c8ddfc047dd5e6f750fb52c19a9
jaydeepw <jaydeep.w@gmail.com>
2019-07-04 19:43:59 +0530

    refactor: Fix menu repeatition on orientation change

--- MatchFilter/app/src/main/java/com/github/jaydeepw/matchfilter/views/MainFragment.kt
+++ MatchFilter/app/src/main/java/com/github/jaydeepw/matchfilter/views/MainFragment.kt
@@ -140,7 +140,9 @@ class MainFragment : BaseFragment() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        inflater?.inflate(R.menu.menu_main, menu)
+        if (menu?.findItem(R.id.action_filter) == null) {
+            inflater?.inflate(R.menu.menu_main, menu)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
