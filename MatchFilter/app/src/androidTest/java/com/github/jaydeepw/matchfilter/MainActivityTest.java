879b388e984a1328c3b522b77cb910e341e23f2b
jaydeepw <jaydeep.w@gmail.com>
2019-07-05 14:52:39 +0530

    chore: Add UI test for main screen

---
+++ MatchFilter/app/src/androidTest/java/com/github/jaydeepw/matchfilter/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.github.jaydeepw.matchfilter;
+
+import android.content.Intent;
+import androidx.test.rule.ActivityTestRule;
+import com.github.jaydeepw.matchfilter.views.MainActivity;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static com.github.jaydeepw.matchfilter.RecyclerViewItemCountAssertion.withItemCount;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.not;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class,false,false);
+
+    @Test
+    public void isAbleToLoadList() {
+        activityRule.launchActivity(new Intent());
+
+        onView(withId(R.id.matchesRecyclerView)).check(matches(isDisplayed()));
+        onView(withId(R.id.messageTextView)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.matchesRecyclerView)).check(withItemCount(greaterThan(1)));
+    }
+}
---
+++ MatchFilter/app/src/androidTest/java/com/github/jaydeepw/matchfilter/MockServerDispatcher.java
@@ -0,0 +1,4 @@
+package com.github.jaydeepw.matchfilter;
+
+public class MockServerDispatcher {
+}
